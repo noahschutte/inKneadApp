f784071c878593808e41ea488605952449c12501
Noah Schutte <noahschutte@gmail.com>
2017-03-19 14:25:15 -0600

    react-native link react-native-code-push

--- android/app/src/main/java/com/inkneadapp/MainApplication.java
+++ android/app/src/main/java/com/inkneadapp/MainApplication.java
@@ -26,6 +26,12 @@ public class MainApplication extends Application implements ReactApplication {
   }
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+
+    @Override
+    protected String getJSBundleFile() {
+      return CodePush.getJSBundleFile();
+    }
+
     @Override
     public boolean getUseDeveloperSupport() {
       return BuildConfig.DEBUG;
