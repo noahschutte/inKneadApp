b6a7cf2fe3100f84915098ac029f739875da8972
Jameson Welling <jamowelling@gmail.com>
2017-07-03 20:23:38 -0600

    create boilerplate for notification component

---
+++ src/components/Notification.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View, TouchableOpacity } from 'react-native';
+
+const Notification = (props) => {
+  return (
+    <View />
+  );
+};
+
+export default Notification;
