8eb655cf55e2ad5f19ab30fabec96409390dcc4a
Jameson Welling <jamowelling@gmail.com>
2017-08-09 19:15:21 -0600

    fix navigation issue on instructions scene when moving from step 1 to step 2

--- src/components/Scenes/InstructionsScene.js
+++ src/components/Scenes/InstructionsScene.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import { Clipboard, Linking, View } from 'react-native';
-import { IndicatorViewPager } from 'rn-viewpager';
+import { Clipboard, Linking, View, Text } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 import PlatformText from '../PlatformText';
 import Button from '../Button2';
@@ -17,7 +16,48 @@ class InstructionsScene extends Component {
     copied: false,
     content: '',
     errorMessage: '',
+    subScene: () => this.step1(),
   };
+
+  onPress = () => {
+    this._setClipboardContent();
+    this.setState({ subScene: this.step2 });
+  };
+
+  step1 = () => {
+    return (
+      <View style={styles.pageWrapper}>
+        <PlatformText type='bold' textStyle={styles.stepTextStyle}>Step 1:</PlatformText>
+        <PlatformText type='demi-bold' textStyle={styles.stepTextStyle}>Tap the email below to copy</PlatformText>
+        <PlatformText type='bold' textStyle={[styles.stepTextStyle, styles.email]} onPress={this.onPress}>
+          {this.props.recipientEmail}
+        </PlatformText>
+      </View>
+    );
+  }
+
+  step2 = () => {
+    return (
+      <View style={styles.pageWrapper}>
+        <PlatformText type='bold' textStyle={styles.stepTextStyle}>Step 2:</PlatformText>
+        <PlatformText type='demi-bold' textStyle={styles.stepTextStyle}>
+          Follow the link below and paste the email where it says "recipient email"
+        </PlatformText>
+
+        <Button
+          onPress={() => this.handleVendorSite(vendors[this.props.entry.vendor])}
+          touchableOpacity
+          buttonStyle={{ backgroundColor: '#ce0000', marginVertical: 25 }}
+          textStyle={styles.hyperlink}
+        >
+          {this.props.entry.vendor}
+        </Button>
+        <PlatformText type='demi-bold' textStyle={{ fontSize: 24, color: '#000', textAlign: 'center' }}>
+          And wait for them to receive your awesome gift!
+        </PlatformText>
+      </View>
+    );
+  }
 
   handleVendorSite = vendorURL => {
     Linking.openURL(vendorURL);
@@ -35,54 +75,9 @@ class InstructionsScene extends Component {
   };
 
   render() {
-    const { stepTextStyle, email, hyperlink } = styles;
-
-    const onPress = () => {
-      this._setClipboardContent();
-      console.log('this.props', this.props);
-      Actions.InstructionsScene({
-        page: 1,
-        entry: {
-          vendor: 'Dominos',
-          pizzas: 2,
-        }
-      });
-    };
     return (
       <View style={{ flex: 1 }}>
-        <IndicatorViewPager
-          style={{ flex: 1 }}
-          initialPage={this.props.page}
-        >
-
-          <View style={styles.pageWrapper}>
-            <PlatformText type='bold' textStyle={stepTextStyle}>Step 1:</PlatformText>
-            <PlatformText type='demi-bold' textStyle={stepTextStyle}>Tap the email below to copy</PlatformText>
-            <PlatformText type='bold' textStyle={[stepTextStyle, email]} onPress={onPress}>
-              {this.props.recipientEmail}
-            </PlatformText>
-          </View>
-
-          <View style={styles.pageWrapper}>
-            <PlatformText type='bold' textStyle={stepTextStyle}>Step 2:</PlatformText>
-            <PlatformText type='demi-bold' textStyle={stepTextStyle}>
-              Follow the link below and paste the email where it says "recipient email"
-            </PlatformText>
-
-            <Button
-              onPress={() => this.handleVendorSite(vendors[this.props.entry.vendor])}
-              touchableOpacity
-              buttonStyle={{ backgroundColor: '#ce0000', marginVertical: 25 }}
-              textStyle={hyperlink}
-            >
-              {this.props.entry.vendor}
-            </Button>
-            <PlatformText type='demi-bold' textStyle={{ fontSize: 24, color: '#000', textAlign: 'center' }}>
-              And wait for them to receive your awesome gift!
-            </PlatformText>
-          </View>
-
-        </IndicatorViewPager>
+          {this.state.subScene()}
       </View>
     );
   }
